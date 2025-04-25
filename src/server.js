234bd04d43b9291dda9d46050599649b969e657c
yacine <yacineyacine31212@gmail.com>
2025-04-25 17:52:09 +0100

    serve the index.html file for .get/ route

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __direname = dirname(__filename);
+
+app.use(express.static(path.join(__direname, "../public")));
+
 app.get("/", (req, res) =>{
-    res.send("hello world");
+    res.sendFile(path.join(__direname, "public", "index.html"));
 })
 
 app.listen(PORT, () =>{
